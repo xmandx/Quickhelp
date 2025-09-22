d184be6e3a38c14d8557d1e1119cd2efd887e0c0
xmandx <amanda.godinho@etec.sp.gov.br>
2025-09-22 09:03:11 -0300

    páginas de cadastro

--- script/pass.js
+++ script/pass.js
@@ -12,3 +12,20 @@ btn_input.addEventListener('click', function () {
         img_input.src = "../assets/icon/mostrar.png";
     }
 })
+
+// confirmar senha
+
+var input2 = document.getElementById("cpassword");
+var btn_input2 = document.getElementById("btn-pass2");
+var img_input2 = btn_input2.querySelector("img");
+
+btn_input2.addEventListener('click', function () {
+    console.log("teste 1");
+    if (img_input2.src.includes("mostrar.png")) {
+        input2.type = "text";
+        img_input2.src = "../assets/icon/ocultar.png";
+    }else if (img_input2.src.includes("ocultar.png")) {
+        input2.type = "password";
+        img_input2.src = "../assets/icon/mostrar.png";
+    }
+})
