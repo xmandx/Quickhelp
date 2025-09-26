4071e230ba5eaf0590c7d80e9b15e962c814838c
xmandx <amanda.godinho@etec.sp.gov.br>
2025-09-26 15:15:41 -0300

    página inicial de usuário

--- script/tema.js
+++ script/tema.js
@@ -4,9 +4,15 @@ var body = document.body;
 
 window.addEventListener('load', function () {
     if (localStorage.getItem("tema") == "dark") {
-        body.classList.add("dark");
-        img.src = "../assets/icon/lua-vermelha.png";
-        img.alt = "Tema claro";
+        if (window.location.href.includes("usuario.html")) {
+            body.classList.add("dark");
+            img.src = "../assets/icon/lua-branca.png";
+            img.alt = "Tema claro";
+        }else{
+            body.classList.add("dark");
+            img.src = "../assets/icon/lua-vermelha.png";
+            img.alt = "Tema claro";
+        }
     }
 });
 
@@ -19,6 +25,19 @@ btn.addEventListener('click', function () {
 
     } else if (img.src.includes("lua-vermelha.png")) {
         img.src = "../assets/icon/sol-vermelho.png";
+        img.alt = "Tema escuro";
+        body.classList.remove("dark");
+        localStorage.setItem("tema", "light")
+    }
+
+    if (img.src.includes("sol-branco.png")) {
+        img.src = "../assets/icon/lua-branca.png";
+        img.alt = "Tema claro";
+        body.classList.add("dark");
+        localStorage.setItem("tema", "dark")
+
+    } else if (img.src.includes("lua-branca.png")) {
+        img.src = "../assets/icon/sol-branco.png";
         img.alt = "Tema escuro";
         body.classList.remove("dark");
         localStorage.setItem("tema", "light")
